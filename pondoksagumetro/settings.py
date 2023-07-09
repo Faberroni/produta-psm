9949332bb62b3cde2a602902cafb7ee1247c78a6
myusufraizala <raizal20ashari@apps.ipb.ac.id>
2023-07-09 17:04:35 +0700

    settings changes

--- pondoksagumetro/settings.py
+++ pondoksagumetro/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://pondokmetrosagu.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://pondokmetrosagu.up.railway.app']
 
 ROOT_URLCONF = 'pondoksagumetro.urls'
 
