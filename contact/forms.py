88cdbc510aeb4bab4ccff91c210f243b5b4e6f1a
myusufraizala <raizal20ashari@apps.ipb.ac.id>
2023-07-10 22:26:01 +0700

    UI Changes

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,22 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True, max_length=100)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    from_email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class':'form-control',
+        'placeholder':'Masukkan email anda',
+        },
+        ),
+        required=True)
+    subject = forms.CharField(widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'placeholder':'Masukkan subjek pesan'
+        }
+        ),
+        required=True, 
+        max_length=100)
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'class':'form-control',
+        'placeholder':'Masukkan pesan yang ingin disampaikan!'
+        }
+        ),
+        required=True)
