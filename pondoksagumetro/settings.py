b46b074c1bf53533c16084d63e4049d2376b8265
myusufraizala <raizal20ashari@apps.ipb.ac.id>
2023-07-20 16:47:52 +0700

    Added Product Catalog

--- pondoksagumetro/settings.py
+++ pondoksagumetro/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'contact',
+    'shop.apps.ShopConfig'
 ]
 
 MIDDLEWARE = [
--- pondoksagumetro/urls.py
+++ pondoksagumetro/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('contact/', include("contact.urls")),
+    path('shop/', include('shop.urls',))
 ]
